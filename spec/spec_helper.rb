1b2cfeb93ea2e3f6d50bae2c71008b926adfb317
stujo <stujo@imagescript.com>
2014-04-17 15:49:22 -0700

    Removing Duplicate / Comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,4 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-  #config.extend VCR::RSpec::Macros
-  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
