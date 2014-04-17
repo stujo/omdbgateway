1365ebc5131a67cfdccbf231c2a57207febdb8a8
stujo <stujo@imagescript.com>
2014-04-17 12:51:04 -0700

    Trying ResponseWrapper in faraday to standardize return type

--- lib/omdbapi/gateway.rb
+++ lib/omdbapi/gateway.rb
@@ -7,15 +7,86 @@ module OMDB
   class Gateway
     attr_reader :base_uri
 
+    class ResponseWrapper
+      attr_reader :http_status, :body, :error_message
+
+      def initialize body, http_status = 200, http_error_message = nil
+        @http_status = http_status
+        @body = body
+        @app_failed = false
+        if http_success?
+          if @body.instance_of?(Hash)
+            if @body.key?('Response') && @body['Response'] == 'False'
+              @app_failed = true
+              if @body.key?('Error')
+                @error_message = @body['Error']
+              end
+            end
+          end
+        else
+          @error_message = http_error_message
+        end
+      end
+
+      def http_success?;
+        @http_status >= 200 && @http_status < 300
+      end
+
+      def app_success?
+        !@app_failed
+      end
+
+      def success?;
+        http_success? && app_success?
+      end
+
+      def element tag, default = nil
+        if success? && body.instance_of?(Hash) && body.key?(tag)
+          body[tag]
+        else
+          default
+        end
+      end
+
+      def length
+        if success? && body.instance_of?(Array) && !body.empty?
+          body.length
+        else
+          0
+        end
+      end
+
+      def first default = nil
+        if success? && body.instance_of?(Array) && !body.empty?
+          body[0]
+        else
+          default
+        end
+      end
+    end
+
+
+    class ResponsePatcher < Faraday::Response::Middleware
+      def call(env)
+        begin
+          @app.call(env)
+          ResponseWrapper.new(env[:body], env[:status])
+        rescue Faraday::Error::ConnectionFailed => e
+          ResponseWrapper.new(nil, 500, "Connection Failed: #{e}")
+        end
+      end
+    end
+
     if Faraday.respond_to? :register_middleware
       Faraday.register_middleware :response,
-                                  :omdb => lambda { ResponseMiddleware }
+                                  :omdb_gateway => lambda { ResponsePatcher }
     end
 
     def initialize base_uri
       @base_uri = base_uri
       @conn = Faraday.new(:url => @base_uri) do |faraday|
         faraday.request :url_encoded # form-encode POST params
+        faraday.response :omdb_gateway
         faraday.response :json
         faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
       end
@@ -38,7 +109,7 @@ module OMDB
         req.params[:y] = year unless year.nil?
       end
       # Middleware creates the Hash
-      response.body
+      response
     end
 
     # Retrieves a movie or show based on its title.
@@ -52,14 +123,9 @@ module OMDB
         req.url '/'
         req.params = {s: q}
       end
-      # Middleware creates the Hash
-      results = response.body
-      if results.key? 'Search'
-        results['Search']
-      else
-        []
-      end
+      response.element('Search', [])
     end
+
 
     # Retrieves a movie or show based on its IMDb ID.
     #
@@ -72,11 +138,7 @@ module OMDB
         req.url '/'
         req.params = {i: imdb_id}
       end
-      if response.body.key? 'Error'
-        nil
-      else
-        response.body
-      end
+      response
     end
   end
 end
--- spec/omdbapi/gateway_spec.rb
+++ spec/omdbapi/gateway_spec.rb
@@ -15,11 +15,11 @@ describe OMDB::Gateway do
       end
 
       it 'should return a hash of movie attributes' do
-        title_results.should be_instance_of Hash
+        title_results.should be_instance_of OMDB::Gateway::ResponseWrapper
       end
 
       it 'should contain a title' do
-        title_results['Title'].should be_instance_of String
+        title_results.element('Title').should be_instance_of String
       end
     end
 
@@ -28,7 +28,7 @@ describe OMDB::Gateway do
       let(:title_results2) { gateway.title_search('True Grit', '1969') }
 
       it 'should not be the same title' do
-        title_results1.should_not eq(title_results2[0])
+        title_results1.body.should_not eq(title_results2.body)
       end
     end
 
@@ -37,23 +37,25 @@ describe OMDB::Gateway do
       let(:title_results2) { gateway.title_search('Game of Thrones', nil, 'full') }
 
       it 'should have different plots' do
-        title_results1['Plot'].should_not eq(title_results2['Plot'])
+        title_results1.element('Plot').should_not eq(title_results2.element('Plot'))
       end
     end
 
     describe 'with a movie that doesn' 't exist' do
-      let(:no_title_results) { gateway.title_search('lsdfoweirjrpwef323423dsfkip') }
+      let(:no_title_results) {
+          gateway.title_search('lsdfoweirjrpwef323423dsfkip')
+      }
 
       it 'should return a hash' do
-        no_title_results.should be_instance_of Hash
+        no_title_results.should be_instance_of OMDB::Gateway::ResponseWrapper
       end
 
       it 'should return a hash with a false response' do
-        no_title_results['Response'].should eq('False')
+        no_title_results.success?.should eq false
       end
 
       it 'should return a hash with an error message' do
-        no_title_results['Error'].should be_instance_of String
+        no_title_results.error_message.should be_instance_of String
       end
     end
   end
@@ -61,14 +63,16 @@ describe OMDB::Gateway do
   describe '#find_by_id' do
 
     describe 'with a title that exists' do
-      let(:id_lookup) { gateway.find_by_id('tt0411008') }
+      let(:id_lookup) {
+        gateway.find_by_id('tt0411008')
+      }
 
       it 'should return a hash of movie attributes' do
-        id_lookup.should be_instance_of Hash
+        id_lookup.should be_instance_of OMDB::Gateway::ResponseWrapper
       end
 
       it 'should contain a title' do
-        id_lookup['Title'].should be_instance_of String
+        id_lookup.element('Title').should be_instance_of String
       end
     end
 
@@ -76,7 +80,7 @@ describe OMDB::Gateway do
       let(:no_id_lookup) { gateway.find_by_id('tt1231230123') }
 
       it 'should return nil' do
-        no_id_lookup.should eq nil
+        no_id_lookup.success?.should eq false
       end
     end
   end
@@ -91,24 +95,30 @@ describe OMDB::Gateway do
       end
 
       it 'should return an array with hash contents' do
-        results[0].should be_instance_of Hash
+        results.first.should be_instance_of Hash
       end
     end
 
     describe 'with a single search result' do
       let(:result) { gateway.free_search('Star Wars: Episode VI - Return of the Jedi') }
 
-      it 'should return a array of items with 1 element' do
+      it 'should return an array' do
         result.should be_instance_of Array
       end
 
+      it 'should return an array with 1 element' do
+        result.length.should eq 1
+      end
+
       it 'should have a title' do
-        result[0]['Title'].should eq('Star Wars: Episode VI - Return of the Jedi')
+        result.first['Title'].should eq('Star Wars: Episode VI - Return of the Jedi')
       end
     end
 
     describe 'with no search results' do
-      let(:results) { gateway.free_search('lsdfoweirjrpwef323423dsfkip') }
+      let(:results) {
+        gateway.free_search('lsdfoweirjrpwef323423dsfkip')
+      }
 
       it 'should return an empty array' do
         results.should be_instance_of Array
